7e3d4062a4010990ad1eeae5420653344384437e
forcecore <ssanzing@gmail.com>
2021-08-16 19:35:26 +0900

    Initialize project

---
+++ resource.h
@@ -0,0 +1,18 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// KiwoomRestfulCpp.rc에서 사용되고 있습니다.
+//
+#define IDD_KIWOOMRESTFULCPP_DIALOG     102
+#define IDR_MAINFRAME                   128
+#define IDC_KHOPENAPICTRL1              1000
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
